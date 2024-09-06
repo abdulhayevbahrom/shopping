af654c0f4618a56ced749b6a932c6820e9507691
bekzodbekbozorboyev <bekzodbozorboyev12@gmail.com>
2024-09-06 16:55:33 +0500

    add  links

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>App.js</h1>
       <h1>App.js</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, adipisci ex suscipit dolore voluptatum nam tenetur laudantium voluptatibus officiis deserunt.</p>
+      <a href="/">Learn more</a>
     </div>
   );
 }
